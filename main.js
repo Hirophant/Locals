cee687d48623c638754540c523b3ec8bf484c6dc
JJ <johnjohnsonr@gmail.com>
2023-02-10 02:47:52 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,69 @@
+var cb = document.createElement('input');
+cb.type = 'checkbox';
+cb.id = 'chat-img-block';
+cb.name = 'chat-img-block';
+
+var label = document.createElement('label');
+label.for = 'chat-img-block';
+
+
+label.textContent = 'Block Images'
+label.prepend(cb);
+label.style = "float: left; padding: 24px 0px;"
+
+document.querySelector('.logo-rr.logo-community').insertAdjacentElement('afterend', label)
+
+var cb = document.createElement('input');
+cb.type = 'checkbox';
+cb.id = 'chat-mv-img-block';
+cb.name = 'chat-mv-img-block';
+
+var label = document.createElement('label');
+label.for = 'chat-mv-img-block';
+
+
+label.textContent = 'Block Moving Images'
+label.prepend(cb);
+label.style = "float: left; padding: 24px 5px;"
+
+document.querySelector('.logo-rr.logo-community').insertAdjacentElement('afterend', label)
+
+var observer = new MutationObserver(callback)
+
+function callback(mutationList, observer) {
+    if (document.getElementById('chat-img-block').checked || document.getElementById('chat-mv-img-block').checked) {
+        for (mut of mutationList) {
+            if (mut.type == 'childList') {
+                Array.from(mut.addedNodes).forEach(node => {
+                    if (node.nodeName != '#text') {
+                        if(node.querySelector('.message-photo')) {
+                            Array.from(node.querySelectorAll('.message-photo')).forEach(node => {
+                                if (document.getElementById('chat-mv-img-block').checked && node.querySelector('a').href?.endsWith('gif')) {
+                                    node.outerHTML = '[img removed]'
+                                    console.log(`Removed photo`)
+                                    return
+                                }
+                                if (document.getElementById('chat-img-block').checked) {
+                                    node.outerHTML = '[img removed]';
+                                    console.log(`Removed photo`);
+                                }
+                                
+                                
+                            })
+                        }
+                    }
+                })
+            }
+            
+        }
+    }
+}
+
+
+observer.observe(
+    document.getElementById('chat-history'),
+    {
+        subtree: true,
+        childList: true
+    }
+)
